b8913350a0844e3b0fc57d543d2d21e96b7ef7c1
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-04-12 17:49:50 +0200

    modifiche generali

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import './index.css';
+import Home from './screens/Home';
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+  <Home />
 );
+
