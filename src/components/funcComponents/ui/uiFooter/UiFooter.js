0836436a6c864518d3a6f486af8cf8fd4bbb0011
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-04-13 11:33:13 +0200

    add footer

---
+++ src/components/funcComponents/ui/uiFooter/UiFooter.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+
+// css 
+import './UiFooter.css'
+
+function UiFooter() {
+
+    return (
+        <footer>
+            <div className="footer_container">
+                <div className="info_footer">
+                    <ul>
+                        News
+                        <li>
+                            Lorem
+                        </li>
+                        <li>
+                            ipsum
+                        </li>
+                        <li>
+                            Lorem
+                        </li>
+                    </ul>
+                    <ul>
+                        Scopri di pi&ugrave;
+                        <li>
+                            ipsum
+                        </li>
+                        <li>
+                            Lorem ipsum
+                        </li>
+                        <li>
+                            Lorem Lorem
+                        </li>
+                        <li>
+                            Lorem ipsum dol
+                        </li>
+                        <li>
+                            Lorem
+                        </li>
+                    </ul>
+
+                    <ul>
+                        Contatti
+                        <li>
+                            Lorem
+                        </li>
+
+                        <li>
+                            loool
+                        </li>
+                    </ul>
+                </div>
+
+
+                <ul className="socialmedia">
+                    <li>
+                        Facebook
+                    </li>
+                    <li>
+                        twitter
+                    </li>
+                    <li>
+                        Instagram
+                    </li>
+                </ul>
+            </div>
+            <div className="copyright"> &copy; 2007 - 2022, The Big Bang Theory Fan Site. A fan site unaffiliated with Chuck Lorre Productions or Warner Bros. </div>
+        </footer>
+    )
+}
+
+
+
+
+export default UiFooter
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,11 @@
 import GameTable from '../components/classComponents/GameTable';
 import './Home.css';
-
+import UiFooter from '../components/funcComponents/ui/uiFooter/UiFooter';
 function Home() {
   return (
     <div className="Home">
       <GameTable />
+      <UiFooter />
     </div>
   );
 }
