60577d41f9230d6136d8e5a7a3ed9bbdd441115b
NicolaL95 <nicola.lilla@gmail.com>
2022-04-13 12:50:06 +0200

    Add header fixed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,19 +5,22 @@ import UiHeader from "../components/funcComponents/ui/uiButton/uiHeader/UiHeader
 //images
 import bg_image from '../assets/images/tbbt_bg_image.jpg'
 
+
 function Home() {
   return (
     <div className="Home">
       <UiHeader />
       <main>
         <div className="jumbotron" style={{ backgroundImage: `url(${bg_image})` }}></div>
-        <div className="container">
-          <h1>Ti ringraziamo per aver acquistato dal nostro shop!</h1>
-          <h2>Sheldon vuole sfidarti al suo gioco preferito: Sasso-carta-forbici-lizard-Spock! Accetti la sfida?</h2>
-          <p className='game_info'>Le regole sono semplici: le forbici tagliano la carta; la carta copre la pietra; la pietra schiaccia la lucertola; la lucertola avvelena Spock; Spock rompe le forbici; le forbici decapitano la lucertola; la lucertola mangia la carta; la carta invalida Spock; Spock vaporizza il sasso; il sasso rompe le forbici</p>
-          <div>
-            <GameTable />
-            <div className='adv'></div>
+        <div style={{ backgroundColor: 'rgb(88, 88, 196)' }}>
+          <div className="container">
+            <h1>Ti ringraziamo per aver acquistato dal nostro shop!</h1>
+            <h2>Sheldon vuole sfidarti al suo gioco preferito: Sasso-carta-forbici-lizard-Spock! Accetti la sfida?</h2>
+            <p className='game_info'>Le regole sono semplici: le forbici tagliano la carta; la carta copre la pietra; la pietra schiaccia la lucertola; la lucertola avvelena Spock; Spock rompe le forbici; le forbici decapitano la lucertola; la lucertola mangia la carta; la carta invalida Spock; Spock vaporizza il sasso; il sasso rompe le forbici</p>
+            <div>
+              <GameTable />
+              <div className='adv'></div>
+            </div>
           </div>
         </div>
       </main>
