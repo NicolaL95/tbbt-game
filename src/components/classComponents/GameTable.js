be25514ea18136c1e347a20e913d1a7793b011ca
NicolaL95 <nicola.lilla@gmail.com>
2022-04-13 11:28:52 +0200

    Add box shadow

--- src/components/classComponents/GameTable.js
+++ src/components/classComponents/GameTable.js
@@ -1,5 +1,7 @@
 import react, { Component } from "react";
 
+//images
+import bg_image from '../../assets/images/tbbt_bg_image.jpg'
 // css 
 import './GameTable.css'
 
@@ -37,41 +39,49 @@ class GameTable extends Component {
         return (
             <>
                 <UiHeader />
-                <div className="game_table_container">
-                    <div className="user_box">
-                        <UiButton
-                            nameBtn={'carta'}
-                            id={0}
-                            callback={this.playGame}
-                        />
-                        <UiButton
-                            nameBtn={'forbice'}
-                            id={1}
-                            callback={this.playGame}
-                        />
-                        <UiButton
-                            nameBtn={'sasso'}
-                            id={2}
-                            callback={this.playGame}
-                        />
-                        <UiButton
-                            nameBtn={'lizard'}
-                            id={3}
-                            callback={this.playGame}
+                <main>
+                    <div className="jumbotron"></div>
+                    <div className="container">
+                        <h1>Ti ringraziamo per aver acquistato dal nostro shop!</h1>
+                        <h2>Sheldon vuole sfidarti al suo gioco preferito: Sasso-carta-forbici-lizard-Spock! Accetti la sfida?</h2>
+                        <p>Le regole sono semplici: le forbici tagliano la carta; la carta copre la pietra; la pietra schiaccia la lucertola; la lucertola avvelena Spock; Spock rompe le forbici; le forbici decapitano la lucertola; la lucertola mangia la carta; la carta invalida Spock; Spock vaporizza il sasso; il sasso rompe le forbici</p>
+                        <div className="game_table_container">
+                            <div className="user_box">
+                                <UiButton
+                                    nameBtn={'carta'}
+                                    id={0}
+                                    callback={this.playGame}
+                                />
+                                <UiButton
+                                    nameBtn={'forbice'}
+                                    id={1}
+                                    callback={this.playGame}
+                                />
+                                <UiButton
+                                    nameBtn={'sasso'}
+                                    id={2}
+                                    callback={this.playGame}
+                                />
+                                <UiButton
+                                    nameBtn={'lizard'}
+                                    id={3}
+                                    callback={this.playGame}
 
-                        />
-                        <UiButton
-                            nameBtn={'spock'}
-                            id={4}
-                            callback={this.playGame}
-                        />
+                                />
+                                <UiButton
+                                    nameBtn={'spock'}
+                                    id={4}
+                                    callback={this.playGame}
+                                />
 
 
+                            </div>
+                            <div className="cpu_box">
+
+                            </div>
+                        </div>
                     </div>
-                    <div className="cpu_box">
-
-                    </div>
-                </div>
+                </main>
             </>
         )
     }
